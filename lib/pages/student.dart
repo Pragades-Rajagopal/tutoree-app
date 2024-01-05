8ba834abad602394f4f67d2a6208f0377eee6ce6
Pragades-Rajagopal <pragadesgoldsmith@outlook.com>
2024-01-05 23:23:21 +0530

    adds bottom navbar to student page

--- lib/pages/student.dart
+++ lib/pages/student.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tutoree_app/pages/login.dart';
 
@@ -11,22 +12,40 @@ class StudentPage extends StatefulWidget {
 }
 
 class _StudentPageState extends State<StudentPage> {
+  var _currentIndex = 0;
   Future<void> logoutDo() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.remove("token");
     Get.offAll(() => const LoginPage());
   }
 
+  List<String> headers = [
+    'home',
+    'feeds',
+    'profile',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        foregroundColor: Colors.black,
+        title: Title(
+          color: Colors.black,
+          child: Text(
+            headers[_currentIndex],
+          ),
+        ),
+        centerTitle: true,
+      ),
       body: SingleChildScrollView(
-        padding: const EdgeInsets.fromLTRB(0, 140, 0, 0),
+        padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text('Student'),
               GestureDetector(
                 onTap: () {
                   logoutDo();
@@ -42,6 +61,43 @@ class _StudentPageState extends State<StudentPage> {
                     height: 0,
                   ),
                 ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 50.0,
+            vertical: 10.0,
+          ),
+          child: GNav(
+            selectedIndex: _currentIndex,
+            onTabChange: (int index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            backgroundColor: Colors.white,
+            color: Colors.black,
+            activeColor: Colors.black,
+            tabBackgroundColor: Colors.grey.shade300,
+            padding: const EdgeInsets.all(12.0),
+            gap: 1,
+            tabs: const [
+              GButton(
+                icon: Icons.home_filled,
+                text: 'home',
+              ),
+              GButton(
+                icon: Icons.markunread_mailbox_rounded,
+                text: 'feeds',
+              ),
+              GButton(
+                icon: Icons.manage_accounts_rounded,
+                text: 'profile',
               ),
             ],
           ),
